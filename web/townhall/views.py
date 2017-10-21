c1675ed76a5dfc19ef90d1eb3142b2f8e1839aa9
shepard.na <shepard.na@husky.neu.edu>
2017-10-21 11:54:59 -0400

    Profile view completed

--- web/townhall/views.py
+++ web/townhall/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
+from models import AppUser, UserPost
+
 # Create your views here.
 
 
@@ -31,3 +33,33 @@ class HomeView(View):
         template = 'home.html'
         context = {}
         return render(request, template, context)
+
+class ProfileView(View):
+
+    def get(self, request):
+        currentuser = AppUser.objects.get(email=request.user)
+        if (currentuser.is_entrepreneur):
+            template = 'entrepreneurprofile.html'
+            context = {}
+            context['username']= currentuser.get_full_name()
+            context['numideas']= UserPost.objects.filter(currentuser).filter(is_idea=True).count()
+            context['numventures'] = UserPost.objects.filter(currentuser).filter(is_idea=False).count()
+            context['location'] = currentuser.city + ", " + currentuser.country
+            context['bio'] = currentuser.bio
+            context['email'] = currentuser.email
+            context['phonenumber'] = currentuser.phone_number
+            context['website'] = currentuser.website
+            context['interests'] = currentuser.interests
+            context['photo'] = currentuser.profile_image
+            return render(request, template, context)
+        elif():
+            template = 'citizenprofile'
+            context = {}
+            context['username'] = currentuser.get_full_name()
+            context['numideas'] = UserPost.objects.filter(currentuser).filter(is_idea=True).count()
+            context['location'] = currentuser.city + ", " + currentuser.country
+            context['bio'] = currentuser.bio
+            context['email'] = currentuser.email
+            context['phonenumber'] = currentuser.phone_number
+            context['photo'] = currentuser.profile_image
+            return render(request, template, context)
